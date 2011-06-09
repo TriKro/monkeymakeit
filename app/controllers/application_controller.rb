79e9955dd7d6cfa0b8bfd445784c0969cddf5eec
Tristan Kromer <i@mNotaNinja.com>
2011-06-09 15:23:44 -0700

    Add logging in static controller. Added rescue_from recordnotfound. Added current_actor method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    flash[:error] = "That record does not exist!"
+    Activity.add(current_actor, request.request_uri, "Record not Found") # log the Activity
+    redirect_to :back
+  end
+
+  private
+
+  # returns a unique identifier for the currently active user,
+  # regardless if they have an account or not
+  def current_actor
+    current_user || session[:session_id]
+  end
+
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def home
-
+    Activity.add(current_actor, request.request_uri, "Viewed", "Page") # log the Activity
   end
 end
