cd9af75bbe822c0d8a9058f5e4780dd0034d515d
Tristan Kromer <i@mNotaNinja.com>
2011-06-16 16:12:37 -0700

    Added user model and migration.

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User
+
+  validates_presence_of :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,}$/i
+
+end
---
+++ db/migrate/20110616230919_create_user_table.rb
@@ -0,0 +1,16 @@
+class CreateUserTable < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string   :email
+      t.string   :code
+
+      t.timestamps
+    end
+
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
+
