077f788420a8263d228bc4e99c3e300d98f4dc64
Harlan Wood <git@hkw7.org>
2011-06-17 00:42:48 -0700

    Refactor JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
 
-  $('#suggestion_email').focus(function() {
-    if ($('#suggestion_email').val() == "What's your email address?") {
+  var default_text = "What's your email address?";
+
+  function clear_default_text() {
+    if ($('#suggestion_email').val() == default_text ) {
       $('#suggestion_email').css('color', 'black');
       $('#suggestion_email').val('');
     }
+  }
+
+  $('#suggestion_email').focus(function() {
+    clear_default_text();
   });
 
   $('#suggestion_submit').click(function() {
-    if ($('#suggestion_email').val() == "What's your email address?") {
-      $('#suggestion_email').val('');
-    }
+    clear_default_text();
   });
 
   $('#suggestion_email').blur(function() {
     if ($('#suggestion_email').val() == '') {
       $('#suggestion_email').css('color', 'grey');
-      $('#suggestion_email').val("What's your email address?");
+      $('#suggestion_email').val(default_text);
     }
   });
 
