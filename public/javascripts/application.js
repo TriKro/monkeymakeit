b2949b79f2582bd4ccfa06f886646e71bde3bf2f
Harlan Wood <git@hkw7.org>
2011-06-17 00:49:31 -0700

    Copy/paste to autopopulate / clear email fields

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,25 +2,43 @@ $(document).ready(function() {
 
   var default_text = "What's your email address?";
 
-  function clear_default_text() {
-    if ($('#suggestion_email').val() == default_text ) {
-      $('#suggestion_email').css('color', 'black');
-      $('#suggestion_email').val('');
+  function clear_default_text(email_field) {
+    if ($(email_field).val() == default_text ) {
+      $(email_field).css('color', 'black');
+      $(email_field).val('');
     }
   }
 
   $('#suggestion_email').focus(function() {
-    clear_default_text();
+    clear_default_text('#suggestion_email');
   });
 
   $('#suggestion_submit').click(function() {
-    clear_default_text();
+    clear_default_text('#suggestion_email');
   });
 
   $('#suggestion_email').blur(function() {
     if ($('#suggestion_email').val() == '') {
       $('#suggestion_email').css('color', 'grey');
       $('#suggestion_email').val(default_text);
+    }
+  });
+
+
+  default_text = "Enter your email for custom code";
+
+  $('#user_email').focus(function() {
+    clear_default_text('#user_email');
+  });
+
+  $('#user_submit').click(function() {
+    clear_default_text('#user_email');
+  });
+
+  $('#user_email').blur(function() {
+    if ($('#user_email').val() == '') {
+      $('#user_email').css('color', 'grey');
+      $('#user_email').val(default_text);
     }
   });
 
