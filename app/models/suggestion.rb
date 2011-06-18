bafa9ff231acb74d8e3e458623134192e8a1ce89
Harlan Wood <git@hkw7.org>
2011-06-18 04:02:20 -0700

    Shave out a gem: html_massage

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -13,7 +13,7 @@ class Suggestion < ActiveRecord::Base
   after_initialize :generate_text_version
 
   def generate_text_version
-    self.before_text ||= HtmlPage.new(before_html, url).to_text
+    self.before_text ||= HtmlMassage.new( before_html, :source_url => url, :ignored_selectors => IGNORED_SELECTORS ).to_text
     self.after_text ||= self.before_text
   end
 
@@ -22,5 +22,53 @@ class Suggestion < ActiveRecord::Base
                                                            :inline_style => true,
                                                            :granularity => :word)
   end
+
+  IGNORED_SELECTORS = [
+      # general:
+      'div#header',
+      'div.header',
+      '#footer',
+      'div#navigation',
+      'div.navigation',
+      'div#sidebar',
+      'div.sidebar',
+      '#breadcrumbs',
+      '.backfornav',
+
+      # wordpress:
+      'div#comments',
+      'div#comment-section',
+      'div#respond',
+
+      # wikipedia "message boxes" -- metadata such as "requires cleanup":
+      # see http://en.wikipedia.org/wiki/Template:Ambox
+      'table.ambox',
+      'table.tmbox',
+      'table.imbox',
+      'table.cmbox',
+      'table.ombox',
+      'table.fmbox',
+      'table.dmbox',
+
+      # social media sharing:
+      'ul#sharebar',
+      'ul#sharebarx',
+
+      # signup:
+      '#mailchimp_signup_bottom',
+
+      # wikinodes:
+      '.jedi_bar_wikinodes',
+      '.remix_on_wikinodes_tab',
+
+      # Posterous blog
+      'div.editbox',
+      'div.postmeta',
+      'div.tag-listing',
+      'div.posterous_tweet_button',
+      'div.comment-count',
+      'div.col#secondary',
+
+    ].freeze
 
 end
