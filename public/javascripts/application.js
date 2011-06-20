81c386d80d41e47168656b60265c8448c07f3aaf
Kartik K. Agaram <vc@akkartik.com>
2011-06-19 18:50:49 -0700

    Fix color of form hints on initial load; try to keep color values sync'd between css and js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,24 @@
+var darkgrey = '#666', black = '#000'; // keep sync'd with base.sass
+
 $(document).ready(function() {
 
   $('#suggestion_email').focus(function() {
     if ($('#suggestion_email').val() == "What's your email address?" ) {
-      $('#suggestion_email').css('color', 'black');
+      $('#suggestion_email').css('color', black);
       $('#suggestion_email').val('');
     }
   });
 
   $('#suggestion_submit').click(function() {
     if ($('#suggestion_email').val() == "What's your email address?" ) {
-      $('#suggestion_email').css('color', 'black');
+      $('#suggestion_email').css('color', black);
       $('#suggestion_email').val('');
     }
   });
 
   $('#suggestion_email').blur(function() {
     if ($('#suggestion_email').val() == '') {
-      $('#suggestion_email').css('color', 'grey');
+      $('#suggestion_email').css('color', darkgrey);
       $('#suggestion_email').val("What's your email address?");
     }
   });
@@ -24,21 +26,21 @@ $(document).ready(function() {
 
   $('#user_email').focus(function() {
     if ($('#user_email').val() == "Enter your email for custom code" ) {
-      $('#user_email').css('color', 'black');
+      $('#user_email').css('color', black);
       $('#user_email').val('');
     }
   });
 
   $('#user_submit').click(function() {
     if ($('#user_email').val() == "Enter your email for custom code" ) {
-      $('#user_email').css('color', 'black');
+      $('#user_email').css('color', black);
       $('#user_email').val('');
     }
   });
 
   $('#user_email').blur(function() {
     if ($('#user_email').val() == '') {
-      $('#user_email').css('color', 'grey');
+      $('#user_email').css('color', darkgrey);
       $('#user_email').val("Enter your email for custom code");
     }
   });
