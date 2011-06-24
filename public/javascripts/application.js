a122f9ae77ed60f169568d19ed40f21900e21dd4
Tristan Kromer <i@mNotaNinja.com>
2011-06-23 22:52:31 -0700

    Fixed js to clear invitation forms.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,48 @@ var darkgrey = '#666', black = '#000'; // keep sync'd with base.sass
 
 $(document).ready(function() {
 
+  $('#invitation_email').focus(function() {
+    if ($('#invitation_email').val() == "Email" ) {
+      $('#invitation_email').css('color', black);
+      $('#invitation_email').val('');
+    }
+  });
+
+  $('#user_submit').click(function() {
+    if ($('#invitation_email').val() == "Email" ) {
+      $('#invitation_email').css('color', black);
+      $('#invitation_email').val('');
+    }
+  });
+
+  $('#invitation_email').blur(function() {
+    if ($('#invitation_email').val() == '') {
+      $('#invitation_email').css('color', darkgrey);
+      $('#invitation_email').val("Email");
+    }
+  });
+
+  $('#invitation_name').focus(function() {
+    if ($('#invitation_name').val() == "Name" ) {
+      $('#invitation_name').css('color', black);
+      $('#invitation_name').val('');
+    }
+  });
+
+  $('#user_submit').click(function() {
+    if ($('#invitation_name').val() == "Name" ) {
+      $('#invitation_name').css('color', black);
+      $('#invitation_name').val('');
+    }
+  });
+
+  $('#invitation_name').blur(function() {
+    if ($('#invitation_name').val() == '') {
+      $('#invitation_name').css('color', darkgrey);
+      $('#invitation_name').val("Name");
+    }
+  });
+
   $('#signup_email').focus(function() {
     if ($('#signup_email').val() == "Email" ) {
       $('#signup_email').css('color', black);
