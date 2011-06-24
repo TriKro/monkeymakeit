f340bf30c7b9884cd2431f97efacee89c2a532ff
Tristan Kromer <i@mNotaNinja.com>
2011-06-23 22:29:36 -0700

    Adjusted js to clear signups input boxes.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,48 @@ var darkgrey = '#666', black = '#000'; // keep sync'd with base.sass
 
 $(document).ready(function() {
 
+  $('#signup_email').focus(function() {
+    if ($('#signup_email').val() == "Email" ) {
+      $('#signup_email').css('color', black);
+      $('#signup_email').val('');
+    }
+  });
+
+  $('#user_submit').click(function() {
+    if ($('#signup_email').val() == "Email" ) {
+      $('#signup_email').css('color', black);
+      $('#signup_email').val('');
+    }
+  });
+
+  $('#signup_email').blur(function() {
+    if ($('#signup_email').val() == '') {
+      $('#signup_email').css('color', darkgrey);
+      $('#signup_email').val("Email");
+    }
+  });
+
+  $('#signup_name').focus(function() {
+    if ($('#signup_name').val() == "Name" ) {
+      $('#signup_name').css('color', black);
+      $('#signup_name').val('');
+    }
+  });
+
+  $('#user_submit').click(function() {
+    if ($('#signup_name').val() == "Name" ) {
+      $('#signup_name').css('color', black);
+      $('#signup_name').val('');
+    }
+  });
+
+  $('#signup_name').blur(function() {
+    if ($('#signup_name').val() == '') {
+      $('#signup_name').css('color', darkgrey);
+      $('#signup_name').val("Name");
+    }
+  });
+
   $('#suggestion_email').focus(function() {
     if ($('#suggestion_email').val() == "What's your email address?" ) {
       $('#suggestion_email').css('color', black);
