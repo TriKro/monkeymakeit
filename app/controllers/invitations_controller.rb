38a0fe240f3d468a26eef390abe91d25fb463444
Tristan Kromer <i@mNotaNinja.com>
2011-06-23 20:05:40 -0700

    Fixed logging in invitations and signups. Hacked js to show signup instead of remix.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,13 +2,16 @@ class InvitationsController < ApplicationController
 
   def new
     @invite = User.new
+    log_activity(request.request_uri, "Began Creating", "Invitation")
   end
 
   def create
     @user = User.where( params[:user] ).first || User.new( params[:user] )
     @user.inviter = session[:user] rescue nil
     @user.save
+    log_activity(request.request_uri, "Created", "User", @user)
     UserMailer.invite(session[:user].email, params[:user][:email], cookies[:url]).deliver
+    log_activity(request.request_uri, "Sent", "Invitation")
     redirect_to new_invitation_path
   end
 
--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,13 +2,13 @@ class SignupsController < ApplicationController
 
   def new
     @signup = User.new
+    log_activity(request.request_uri, "Began Creating", "User")
   end
 
   def create
     @signup = User.new(params[:user])
     if @signup.save
-# TODO: Fix logging
-#      Activity.add(current_actor, request.request_uri, "Created", "User", @signup) # log the Activity
+      log_activity(request.request_uri, "Created", "User", @signup)
       redirect_to(new_invitation_url, :notice => 'User was successfully created.')
     else
       render :action => "new"
