2976e793ca8b5173bc7c9370ead265f8be93d0ac
Tristan Kromer <i@mNotaNinja.com>
2011-07-02 18:47:45 -0700

    Cleaned up sass in a big way.
    js no longer uses internal variables...adds and remove classes which are in basics.sass
    
    Import sheets correspond to layouts. They import all relevant sass files and that's all.
    Individual sass files correspond to views.
    
    All variables are in variables.sass and MUST BE INCLUDED in all other sheets excepting import only sheets. This eliminates any css compile errors.
    Colors are all defined in variables.
    Mixins are kept on the variables sass sheet. They can be passed parameters and have a global default which can be changed.

--- public/javascripts/invitations.js
+++ public/javascripts/invitations.js
@@ -1,42 +1,38 @@
-var darkgrey = '#666', black = '#000'; // keep sync'd with base.sass
-
 $(document).ready(function() {
 
   $('#invitation_email').focus(function() {
     if ($('#invitation_email').val() == "Email" ) {
-      $('#invitation_email').css('color', black);
+      $('#invitation_email').addClass('black').removeClass('darkgrey');
       $('#invitation_email').val('');
     }
   });
 
   $('#invitation_email').blur(function() {
     if ($('#invitation_email').val() == '') {
-      $('#invitation_email').css('color', darkgrey);
+      $('#invitation_email').addClass('darkgrey').removeClass('black');
       $('#invitation_email').val("Email");
     }
   });
 
   $('#invitation_name').focus(function() {
     if ($('#invitation_name').val() == "Name" ) {
-      $('#invitation_name').css('color', black);
+      $('#invitation_name').addClass('black').removeClass('darkgrey');
       $('#invitation_name').val('');
     }
   });
 
   $('#invitation_name').blur(function() {
     if ($('#invitation_name').val() == '') {
-      $('#invitation_name').css('color', darkgrey);
+      $('#invitation_name').addClass('darkgrey').removeClass('black');
       $('#invitation_name').val("Name");
     }
   });
 
   $('#invitation_submit').click(function() {
     if ($('#invitation_name').val() == "Name" ) {
-      $('#invitation_name').css('color', black);
       $('#invitation_name').val('');
     };
     if ($('#invitation_email').val() == "Email" ) {
-      $('#invitation_email').css('color', black);
       $('#invitation_email').val('');
     }
   });
