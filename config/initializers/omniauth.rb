92643072c36e2e78531aff0dba5cf7bb8d67485d
Tristan Kromer <TK@TristanKromer.com>
2011-07-07 16:19:25 -0700

    Added Facebook app credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '6zSg0vRGrNIYVmBmgewogA', 'Y9hhuIGBtZ5EEynaP0VZV83ljMgeeTuVja1xLynJA'
-  #provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '169349883130377', '21afbece24f2d35bb9f123055cf073af'
 end
