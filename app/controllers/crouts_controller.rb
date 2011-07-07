cff91dfd675a76c6f709b4922f2142ca63f864ea
Tristan Kromer <TK@TristanKromer.com>
2011-07-06 18:07:43 -0700

    Expanded crout template. Fixed header logo spacing.

--- app/controllers/crouts_controller.rb
+++ app/controllers/crouts_controller.rb
@@ -5,6 +5,43 @@ class CroutsController < ApplicationController
   }
 
   def doris
+    @activities = [
+        { :actor => 'Tyler Landry',
+          :image => 'images/image-scott-lambridis.png',
+          :verb => 'added a',
+          :object => 'picture',
+          :details => '"Rough sketch of the mourning crows. These really stick in my head."',
+          :timestamp => 'Sept 3, 2011'
+      },
+      { :actor => 'Tyler Landry',
+          :image => 'images/image-scott-lambridis.png',
+          :verb => 'added a',
+          :object => 'picture',
+          :details => '"Rough sketch of the mourning crows. These really stick in my head."',
+          :timestamp => 'Sept 3, 2011'
+      },
+      { :actor => 'Tyler Landry',
+          :image => 'images/image-scott-lambridis.png',
+          :verb => 'added a',
+          :object => 'picture',
+          :details => '"Rough sketch of the mourning crows. These really stick in my head."',
+          :timestamp => 'Sept 3, 2011'
+      },
+      { :actor => 'Tyler Landry',
+          :image => 'images/image-scott-lambridis.png',
+          :verb => 'added a',
+          :object => 'picture',
+          :details => '"Rough sketch of the mourning crows. These really stick in my head."',
+          :timestamp => 'Sept 3, 2011'
+      },
+      { :actor => 'Tyler Landry',
+          :image => 'images/image-scott-lambridis.png',
+          :verb => 'added a',
+          :object => 'picture',
+          :details => '"Rough sketch of the mourning crows. These really stick in my head."',
+          :timestamp => 'Sept 3, 2011'
+      }
+    ]
     render 'crout'
   end
 
