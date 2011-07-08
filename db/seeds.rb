a425b46d0fae31ef41dc9c25ca65094ac337a737
Kartik K. Agaram <vc@akkartik.com>
2011-07-07 19:26:05 -0700

    missed seeds when renaming button model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'image_size'
 button_paths = Dir[ Rails.root.join( *%w[ public images widget_buttons * ] ) ]
 button_paths.each do |button_path|
   button_name = button_path.split('/').last
-  invitation = Invitation.where(:button_name => button_name).first || Invitation.new(:button_name => button_name)
+  invitation = Button.where(:button_name => button_name).first || Button.new(:button_name => button_name)
   image_width, image_height = open(button_path, 'rb') { |button_file| ImageSize.new(button_file.read).get_size }
   invitation.button_width  = image_width
   invitation.button_height = image_height / 2   # This assumes all images are sprites, with rollover image stacked below the normal state
@@ -13,4 +13,4 @@ end
 
 # delete invitations for buttons that have been removed
 button_names = button_paths.map{|button_path| button_path.split('/').last }
-Invitation.where("button_name NOT IN (?)", button_names).destroy_all
+Button.where("button_name NOT IN (?)", button_names).destroy_all
