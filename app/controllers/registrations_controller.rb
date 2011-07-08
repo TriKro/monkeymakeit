3ae19d4e43d525de239bf876a2851ccabd9134ec
Harlan Knight Wood <git@hkw7.org>
2011-07-07 20:47:35 -0700

    When user tries to sign up with invalid email, show validation errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,25 @@
 class RegistrationsController < ApplicationController
 
-  def create
-    @user = User.find_by_email(params[:user][:email]) || User.create(params[:user])
-    return redirect_to(root_path) if @user.new_record? # error
-    log_activity(request.request_uri, "Created", "User", @signup)
+  def new
+  end
 
-    session[:user] = @signup
-    log_activity(request.request_uri, "Registered", "User", @signup)
-    redirect_to(registration_thanks_url)
+  def create
+    @user = User.find_by_email(params[:user][:email])
+    unless @user
+      @user = User.new (params[:user])
+      if @user.save
+        log_activity(request.request_uri, "Created", "User", @user)
+        current_user = @user
+        p 111, @current_user  # sanity check
+        return redirect_to root_path, :notice => 'Thanks, we are working hard and will let you know when more awesomeness is ready for you.'
+      else
+        return redirect_to new_registration_path(:user => params[:user]), :alert => @user.errors.full_messages.first
+      end
+    end
+
+#    log_activity(request.request_uri, "Registered", "User", @signup)
+#    redirect_to(registration_thanks_url)
+
   end
 
   def thanks
