0f64fb6173439b918337029cc1da9dfedd40277c
Kartik K. Agaram <vc@akkartik.com>
2011-07-07 20:56:55 -0700

    clicking on buttons logs an activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,11 @@ class ActivitiesController < ApplicationController
     @activities = Activity.all.paginate(:page => params[:page], :per_page => 20)
   end
 
+  def create
+    Activity.create :url => params[:url], :activity_type => params[:activity_type], :target_model => params[:target_model], :target => params[:target], :subtarget_model => params[:subtarget_model]
+    render :text => 'ok'
+  end
+
   def destroy
     @activity = Activity.find(params[:id])
     @activity.destroy
