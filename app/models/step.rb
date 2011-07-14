e01a8a9f1bbe780ae224c0c2fc4184a749f3d34c
Tristan Kromer <TK@TristanKromer.com>
2011-07-14 11:58:06 -0700

    Delete funnels and steps in favor of using Kissmetrics.

--- app/models/step.rb
+++
@@ -1,9 +0,0 @@
-class Step < ActiveRecord::Base
-
-  belongs_to :funnel
-  has_many :activities, :as => :target
-  has_many :activities, :as => :subtarget
-
-  validates_presence_of :sequence
-
-end
