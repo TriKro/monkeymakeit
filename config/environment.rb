6c116d40387dacecec3fd99767776e977812aa3a
Harlan Knight Wood <git@hkw7.org>
2011-07-14 14:35:34 -0700

    Automate the configuration of all sass compiling to the expected css location

--- config/environment.rb
+++ config/environment.rb
@@ -27,16 +27,6 @@ SITE_NAME = "MonkeyMake.it"
 SITE_TITLE = "MonkeyMakeIt"
 
 # Set Sass to compile from Views folder to "public/stylesheets"
-# TODO: Write a routine to automatically create this array. Otherwise has to be updated manually.
-Sass::Plugin.options[:template_location] = {
-    "./app/views/activities" => "./public/stylesheets",
-    "./app/views/admin" => "./public/stylesheets",
-    "./app/views/invitations" => "./public/stylesheets",
-    "./app/views/layouts" => "./public/stylesheets",
-    "./app/views/landing_pages" => "./public/stylesheets",
-    "./app/views/registrations" => "./public/stylesheets",
-    "./app/views/signups" => "./public/stylesheets",
-    "./app/views/static" => "./public/stylesheets",
-    "./app/views/crouts" => "./public/stylesheets",
-    "./app/views/shared" => "./public/stylesheets"
-}
+view_dirs = Dir[ Rails.root.join( 'app/views/*' ) ]
+template_locations = view_dirs.inject( {} ){ |mapping, view_dir| mapping.merge!( view_dir => Rails.root.join( 'public/stylesheets' ).to_s ) }
+Sass::Plugin.options[:template_location] = template_locations
