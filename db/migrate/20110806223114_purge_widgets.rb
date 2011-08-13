86eafb81b65123e8ca9ddda6dbae79340e986401
Tristan Kromer <TK@TristanKromer.com>
2011-08-13 13:54:07 -0700

    Modify migrations: Droppoing tables that don't exist due to deleted migrations.

--- db/migrate/20110806223114_purge_widgets.rb
+++ db/migrate/20110806223114_purge_widgets.rb
@@ -1,9 +1,9 @@
 class PurgeWidgets < ActiveRecord::Migration
   def self.up
     remove_column :users, :random_key
-    drop_table :buttons
-    drop_table :suggestions
-    drop_table :widgets
+    #drop_table :buttons
+    #drop_table :suggestions
+    #drop_table :widgets
   end
 
   def self.down
