0f618bfebc7faf7bad92fc7a1853bd8807676172
Tristan Kromer <TK@TristanKromer.com>
2011-08-27 14:44:40 -0700

    Log contact messages as activity.

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -12,6 +12,7 @@ class ContactMessagesController < ApplicationController
     @contact_message = ContactMessage.new(params[:contact_message])
     if @contact_message.valid?
       UserMailer.contact_us(@contact_message).deliver
+      km.record('activity', { 'type' => 'contact message', 'from' => @contact_message.sender_email })
       flash[:success] = 'Your message has been sent! We\'ll reply as soon as possible.'
       redirect_to(contact_thanks_url)
     else
