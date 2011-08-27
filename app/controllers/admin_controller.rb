93756fa05e9147aae9208dc9abce4eb9be7b3801
Tristan Kromer <TK@TristanKromer.com>
2011-08-27 14:17:56 -0700

    Added basic nav to admin. Fixed set experiment.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,6 @@ class AdminController < ApplicationController
 
   def set_experiment
     session[:monkey_experiment_id] = params[:experiment]
-    redirect_to select_experiment_path
   end
 
 end
