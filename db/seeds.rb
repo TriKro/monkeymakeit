634f4850358234dfcebdc4ea4486683be1fc6323
Tristan Kromer <TK@TristanKromer.com>
2011-08-27 21:13:55 -0700

    Fix seeds...again.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,42 +17,42 @@ require 'image_size'
 
 ActiveRecord::Observer.disable_observers
 
-user = User.find_or_create_by_email(:full_name => "Tristan Kromer",
-                             :email => "accounts@tristankromer.com",
-                             :avatar_remote_url => "http://a2.twimg.com/profile_images/1092489881/24208_1328078796346_1062228442_1001804_2392997_n_normal.jpg",
-                             :access => "admin")
+user = User.find_or_create_by_email(:email => "accounts@tristankromer.com")
+user.update_attributes(:full_name => "Tristan Kromer",
+                       :avatar_remote_url => "http://a2.twimg.com/profile_images/1092489881/24208_1328078796346_1062228442_1001804_2392997_n_normal.jpg",
+                       :access => "admin")
 
-Authentication.find_or_create_by_token(:token => "169349883130377|afbafe5fc656fd73cd682a0f.1-1062228442|nOnsUDtKq2RHHL01ocsuCrkGlwk",
-                                       :secret => "vx3Dfq5RtYgU4sYlQqizWzk227zhdF5114hrJ8Ct18",
-                                       :uid => "31536253",
-                                       :provider => "twitter",
-                                       :user_id => user.id)
+auth = Authentication.find_or_create_by_token(:token => "169349883130377|afbafe5fc656fd73cd682a0f.1-1062228442|nOnsUDtKq2RHHL01ocsuCrkGlwk")
+auth.update_attributes(:secret => "vx3Dfq5RtYgU4sYlQqizWzk227zhdF5114hrJ8Ct18",
+                       :uid => "31536253",
+                       :provider => "twitter",
+                       :user_id => user.id)
 
-Authentication.find_or_create_by_token(:token => "31536253-CaPDpW9iV3HdLZV1HIRC25JS3MMDYXxdx1XVYZOr1",
-                                       :uid => "1062228442",
-                                       :provider => "facebook",
-                                       :user_id => user.id)
+auth = Authentication.find_or_create_by_token(:token => "31536253-CaPDpW9iV3HdLZV1HIRC25JS3MMDYXxdx1XVYZOr1")
+auth.update_attributes(:uid => "1062228442",
+                       :provider => "facebook",
+                       :user_id => user.id)
 
-user = User.find_or_create_by_email(:full_name => "Kartik Agaram",
-                             :email => "twitter@akkartik.com",
-                             :avatar_remote_url => "http://a2.twimg.com/profile_images/42109872/akkartik-sq_normal.JPG",
-                             :access => "admin")
+user = User.find_or_create_by_email(:email => "twitter@akkartik.com")
+user.update_attributes(:full_name => "Kartik Agaram",
+                       :avatar_remote_url => "http://a2.twimg.com/profile_images/42109872/akkartik-sq_normal.JPG",
+                       :access => "admin")
 
-Authentication.find_or_create_by_token(:token => "169349883130377|66d29be0c659fe1fb3226606.1-7951553|7V4bTUXbTdy1jq7SFVBhQNrULjQ",
-                                       :uid => "7951553",
-                                       :provider => "facebook",
-                                       :user_id => user.id)
+auth = Authentication.find_or_create_by_token(:token => "169349883130377|66d29be0c659fe1fb3226606.1-7951553|7V4bTUXbTdy1jq7SFVBhQNrULjQ")
+auth.update_attributes(:uid => "7951553",
+                       :provider => "facebook",
+                       :user_id => user.id)
 
-user = User.find_or_create_by_email(:full_name => "Scott Lambridis",
-                             :email => "scott@omnibucket.com",
-                             :avatar_remote_url => "/images/avatars/image-scott-lambridis.png",
-                             :bio => "My stories have appeared in Storyglossia, the UK's Black Static, and Transfer Magazine. Born and raised in New York, I earned a degree in neurobiology from UVa - which I promptly abandoned for a creative career. I'm completing my MFA at San Francisco State, and working on a book about the scientist who discovered the end of time.",
-                             :access => "admin")
+user = User.find_or_create_by_email(:email => "scott@omnibucket.com")
+user.update_attributes(:full_name => "Scott Lambridis",
+                       :avatar_remote_url => "/images/avatars/image-scott-lambridis.png",
+                       :bio => "My stories have appeared in Storyglossia, the UK's Black Static, and Transfer Magazine. Born and raised in New York, I earned a degree in neurobiology from UVa - which I promptly abandoned for a creative career. I'm completing my MFA at San Francisco State, and working on a book about the scientist who discovered the end of time.",
+                       :access => "admin")
 
-Authentication.find_or_create_by_token(:token => "169349883130377|dd0dec9c68aec02ea07d38b7.1-713411825|XuASGELymbbvxAC01zyIzQmixSM",
-                                       :uid => "713411825",
-                                       :provider => "facebook",
-                                       :user_id => user.id)
+auth = Authentication.find_or_create_by_token(:token => "169349883130377|dd0dec9c68aec02ea07d38b7.1-713411825|XuASGELymbbvxAC01zyIzQmixSM")
+auth.update_attributes(:uid => "713411825",
+                       :provider => "facebook",
+                       :user_id => user.id)
 
 story = Story.find_or_create_by_title(:title => "Oh, Mighty Hiccup!",
                                       :subtitle => "a novel about brothers, hiccups, and the end of time")
