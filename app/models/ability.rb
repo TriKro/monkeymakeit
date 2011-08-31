0a62e39e7bff42093bd06647ef184361d09d0e64
Tristan Kromer <TK@TristanKromer.com>
2011-08-30 23:54:18 -0700

    Update abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,25 +4,30 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user
 
-    # all access
-    if user.access == "reader"
+    can :create, ContactMessage
+    can :read, Story
+    can :referral_redirect, Story
+
+    if user.access == "reader" || user.access == "author"
       can :manage, User, :user_id => user.id
       can :update, Authentication, :user_id => user.id
       can :read, Invite, :user_id => user.id
       can :send_invites, Invite, :user_id => user.id
-    elsif user.access == "author"
+      can :create, Subscription, :user_id => user.id
+      can :destroy, Subscription, :user_id => user.id
+    end
+
+    if user.access == "author"
       can :manage, User, :user_id => user.id
       can :update, Authentication, :user_id => user.id
       can :manage, Story, :user_id => user.id
       can :create, Story
-    elsif user.access == "admin"
+    end
+
+    if user.access == "admin"
       can :manage, :all
     end
 
-    # Abilities for all users:
-    can :create, ContactMessage
-    can :read, Story
-    can :referral_redirect, Story
   end
 
 end
