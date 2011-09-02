a97dac51dab36be52bc54ea0d24f9fb7cf6124e3
Tristan Kromer <TK@TristanKromer.com>
2011-09-01 23:55:35 -0700

    Add email sign up on signup form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,22 @@ class SessionsController < ApplicationController
   }, :only => :new
 
   def new
+    @user = User.new if current_user.nil?
   end
 
   def create
-    user = Authentication.find_or_create_from_hash(request.env['omniauth.auth'], current_user)
-    session[:user_id] = user.id # Log the user in
+    hash = request.env['omniauth.auth']
+    if !hash.nil?
+      @user = Authentication.find_or_create_from_hash(hash, current_user)
+    elsif !params[:user].nil? && current_user.nil?
+      @user = User.find_or_create_from_email_and_attributes(params[:user][:email], { :full_name => params[:user][:full_name] })
+      redirect_to :action => "new", :flash => { :alert => "When signing in as an author or admin you must use Twitter or Facebook connect." } and return if @user.access == "author" || @user.access == "admin"
+      render "new" and return if @user.errors.any?
+    else
+      redirect_to :action => failure and return
+    end
+
+    session[:user_id] = @user.id # Log the user in
     flash[:notice] = "Logged in as #{current_user.full_name}"
     redirect_to session[:return_to]
   end
