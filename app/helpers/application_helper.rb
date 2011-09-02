197949d882bbc13b46e4230458310263ad12c55f
Kartik K. Agaram <vc@akkartik.com>
2011-09-01 19:29:37 -0700

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,21 +19,4 @@ module ApplicationHelper
     content_for(:fonts) { fonts_name }
   end
 
-  def collaborate_js_tag(js_tag_params={})
-    %{<script type="text/javascript" src="#{collaborate_js_url(:format => :js)}?#{js_tag_params.to_query}"></script>}
-  end
-
-  def auth_providers
-    ActiveSupport::OrderedHash[
-            :facebook, 'Facebook',
-            :twitter,  'Twitter',
-#            :open_id,  'OpenID',
-    ]
-  end
-
-  def facebook_iframe_src(invite_url)
-    invite_url = ERB::Util.url_encode(invite_url)
-    "http://www.facebook.com/plugins/like.php?href=#{invite_url};send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=recommend&amp;colorscheme=light&amp;font&amp;height=21"
-  end
-
 end
