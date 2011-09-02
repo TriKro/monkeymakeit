10e79070ab73cbbd3dc745c8d225e20efeb95796
Tristan Kromer <TK@TristanKromer.com>
2011-09-01 20:08:01 -0700

    Add separate FB credentials if on staging server.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '6zSg0vRGrNIYVmBmgewogA', 'Y9hhuIGBtZ5EEynaP0VZV83ljMgeeTuVja1xLynJA'
-  provider :facebook, '169349883130377', '21afbece24f2d35bb9f123055cf073af'
+  if  Rails.env.staging?
+    provider :facebook, '278738435485349', '1b9e52f3b4cbe9fc30496275ef51b8bc'
+  else
+    provider :facebook, '169349883130377', '21afbece24f2d35bb9f123055cf073af'
+  end
 end
