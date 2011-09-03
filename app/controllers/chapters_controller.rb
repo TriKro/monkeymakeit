03cdbe5641280500425cd7b3725144be676eca02
Tristan Kromer <TK@TristanKromer.com>
2011-09-03 12:30:06 -0700

    Display the chapter called in show chapter.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -12,7 +12,7 @@ class ChaptersController < ApplicationController
 
   def show
     @story = Story.find(params[:story_id])
-    @chapter = @story.chapters.find_by_index(1)
+    @chapter = @story.chapters.find(params[:id])
     @user = User.new unless current_user
     if current_user && current_user.subscribed?(@story)
       @invite = current_user.invites.find_or_create_by_story_id(@story.id)
