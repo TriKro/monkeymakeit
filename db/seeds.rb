09a938c9973afb803dea192b29264205148eba20
Kartik K. Agaram <vc@akkartik.com>
2011-09-04 16:18:14 -0700

    New chapters for hiccup; moved static chapter data to public/chapters

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ auth.update_attributes(:uid => "713411825",
 
 Chapter.destroy_all
 Creative.destroy_all
-YAML::load(File.open("#{Rails.root}/app/views/stories/index.yml")).each do |story|
+YAML::load(File.open("#{Rails.root}/public/chapters/index.yml")).each do |story|
   story['user'] = User.find_by_email(story['author'])
   story.delete 'author'
   story['chapters'] = story['chapters'].collect do |chapter|
