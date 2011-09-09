a1ee9e418e7e519acc7eae8ee56864b4a809d546
Tristan Kromer <TK@TristanKromer.com>
2011-09-08 18:45:08 -0700

    Remove unique index of email on users tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110904062736) do
+ActiveRecord::Schema.define(:version => 20110909014218) do
 
   create_table "activities", :force => true do |t|
     t.integer   "user_id"
@@ -130,6 +130,6 @@ ActiveRecord::Schema.define(:version => 20110904062736) do
   end
 
   add_index "users", ["cached_slug"], :name => "index_users_on_cached_slug", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
