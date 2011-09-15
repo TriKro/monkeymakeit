58b56be11ffc9cbe14d0357e332f6a57f9b4f04c
Kartik K. Agaram <vc@akkartik.com>
2011-09-15 16:34:14 -0700

    All emails sent; delete hacky one-time code in the notifier. Leave the rescue clause, though.

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -2,12 +2,7 @@ class Notifier
   def self.run(story, chapter)
     story = Story.find(story)
     chapter = story.chapters.find_by_chapter_index(chapter)
-    if story.subscriptions.collect{|x| x.user_id}.find_index(133) != 65
-      puts "DB changed!!!!"
-      return
-    end
-
-    story.subscriptions[66..-1].each do |s|
+    story.subscriptions.each do |s|
       begin
         UserMailer.deliver_new_chapter_email(s.user, story, chapter)
       rescue
