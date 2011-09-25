39adc4883a33a823f92acc584bc2c3ec41f13259
Kartik K. Agaram <vc@akkartik.com>
2011-09-24 19:01:32 -0700

    Fix the process; when we encounter template errors we should be able to see what they were.

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -2,11 +2,13 @@ class Notifier
   def self.run(story, chapter)
     story = Story.find(story)
     chapter = story.chapters.find_by_chapter_index(chapter)
-    story.subscriptions.each do |s|
+    story.subscriptions.reverse.each do |s| # reverse just for dev/staging; first mail shouldn't be from Scott to Scott
       begin
         UserMailer.deliver_new_chapter_email(s.user, story, chapter)
-      rescue
-        puts "Couldn't email user #{s.user.inspect}"
+      rescue ActionView::Template::Error => e
+        puts "Couldn't construct email for user #{s.user.inspect}: #{e.message}"
+      rescue => e
+        puts "Couldn't email user #{s.user.inspect}: #{e}"
       end
     end
   end
