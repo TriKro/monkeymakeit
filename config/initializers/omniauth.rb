8e1c2cc6ad076fa356754c3c6ed0f5910b5d99c3
Kartik K. Agaram <vc@akkartik.com>
2011-09-26 22:39:51 -0700

    Let's try this fix: http://stackoverflow.com/questions/3977303/omniauth-facebook-certificate-verify-failed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '6zSg0vRGrNIYVmBmgewogA', 'Y9hhuIGBtZ5EEynaP0VZV83ljMgeeTuVja1xLynJA'
   if Rails.env.development?
-    provider :facebook, '202810259783506', '6b5257cf836f53c7e4ad51692c571013'
+    provider :facebook, '202810259783506', '6b5257cf836f53c7e4ad51692c571013', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   elsif Rails.env.staging?
-    provider :facebook, '278738435485349', '1b9e52f3b4cbe9fc30496275ef51b8bc'
+    provider :facebook, '278738435485349', '1b9e52f3b4cbe9fc30496275ef51b8bc', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   else
-    provider :facebook, '169349883130377', '21afbece24f2d35bb9f123055cf073af'
+    provider :facebook, '169349883130377', '21afbece24f2d35bb9f123055cf073af', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   end
 end
