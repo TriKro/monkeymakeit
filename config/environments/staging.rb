c37522561b22fdf478f97fd042fe43ab5c10fae2
Kartik K. Agaram <vc@akkartik.com>
2011-09-26 22:21:04 -0700

    show stack traces on staging to help debug facebook auth issue

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 Monkeymakeit::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false # error reports
+  config.consider_all_requests_local       = true # error reports
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.serve_static_assets = false
